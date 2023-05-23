903e5143badc610accaac6163651befe95572cd0
GeePaw Hill <geepawhill@geepawhill.org>
2023-05-23 13:22:09 -0400

    Started Javadoc on Prompt.

--- src/main/java/org/geepawhill/jltk/prompt/Prompt.java
+++ src/main/java/org/geepawhill/jltk/prompt/Prompt.java
@@ -3,6 +3,25 @@ package org.geepawhill.jltk.prompt;
 import java.io.*;
 import java.util.*;
 
+/**
+ * Provides easy and safe collection and validation of user input.
+ * <p>
+ * The Prompt class is used to create prompts on System.out and read whole lines from
+ * System.in, returning those lines as Reply objects which can be used to extract
+ * fundamental java types safely. Unlike java.util.Scanner, it does not throw exceptions on
+ * invalid user input, it just repeats the prompt and tries again.
+ * <p>
+ * The snippet below will prompt the user with "Please enter the row number: " and wait for
+ * the user to type characters and press enter. If the user enters a valid integer, it will
+ * return, and that integer will be the 0th reply value. If the user enters a non-integer, or a
+ * blank line, it will repeat the prompt.
+ *
+ * <code>
+ * Prompt rowPrompt = new Prompt("Please enter the row number: ");
+ * rowPrompt.anyInteger();
+ * int rowNumer = rowPrompt.reply(0).asInteger();
+ * </code>
+ */
 public class Prompt {
     private final String text;
     private final ArrayList<Reply> replies = new ArrayList<>();
@@ -28,7 +47,11 @@ public class Prompt {
         return true;
     }
 
-    private void run(InputStream in, PrintStream out, Checker checker) {
+    public void run(Checker checker) {
+        run(System.in, System.out, checker);
+    }
+
+    public void run(InputStream in, PrintStream out, Checker checker) {
         while (true) {
             out.print(text);
             String response = new Scanner(in).nextLine();
